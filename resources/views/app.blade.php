0a28193661d29994fd0b85a10cf6adab47d8a604
swrdism <swrdism@gmail.com>
2024-08-06 14:23:08 +0800

    vite init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh-tw" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    @vite('resources/js/app.js')
+</head>
+<body>
+</body>
+</html>
+
